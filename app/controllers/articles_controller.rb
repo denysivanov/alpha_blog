69051cd4be86c1c14709f25fa762a1985d0e4d42
Denys Ivanov <denys.ivanov@gmail.com>
2022-12-30 17:55:52 -0500

    Lesson 100 DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
+
   end
   def index
     @articles = Article.all
@@ -11,43 +13,40 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
       render 'new'
     end
-
   end
 
   def update
-    #byebug
-    @article = Article.find(params[:id])
-
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated successfully."
       redirect_to @article
     else
       render 'edit'
     end
-
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
 
-    # if @article.destroy
-    #   flash[:notice] = "Article was destroyed successfully."
-    #   redirect_to articles_path
-    # else
-    #   render 'new'
-    # end
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
