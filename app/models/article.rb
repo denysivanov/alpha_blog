1d36ac67565273da3f58fc1343173693ffec3466
Denys Ivanov <denys.ivanov@gmail.com>
2022-12-30 10:44:41 -0500

    start to show Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
-
+  validates :title, presence: true, length: { minimum: 6, maximum: 100}
+  validates :description, presence: true, length: { minimum: 10, maximum: 300}
+  validates :description, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 end
+
