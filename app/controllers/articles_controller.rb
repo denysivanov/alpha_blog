cec933671c8f23743aa74ca3b0a1fb52903b4d16
Denys Ivanov <denys.ivanov@gmail.com>
2023-01-13 18:20:35 -0500

    138

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = Article.new
+    @obj = Article.new
   end
 
   def edit
@@ -17,37 +17,37 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
-    @article.user = User.first
-    if @article.save
+    @obj = Article.new(article_params)
+    @obj.user = User.first
+    if @obj.save
       flash[:notice] = "Article was created successfully."
-      redirect_to @article
+      redirect_to @obj
     else
       render 'new'
     end
   end
 
   def update
-    if @article.update(article_params)
+    if @obj.update(article_params)
       flash[:notice] = "Article was updated successfully."
-      redirect_to @article
+      redirect_to @obj
     else
       render 'edit'
     end
   end
 
   def destroy
-    @article.destroy
+    @obj.destroy
     redirect_to articles_path
   end
 
   private
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:obj).permit(:title, :description)
   end
 
   def set_article
-    @article = Article.find(params[:id])
+    @obj = Article.find(params[:id])
   end
 end
